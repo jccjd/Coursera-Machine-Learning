52ad328c1c0becb7830033cb586eb2e4a34c6add
jccjd <1340944538@qq.com>
2019-02-18 20:21:15 +0800

    week2

--- week-4/code/线性神经网络_异或.py
+++ week-4/code/线性神经网络_异或.py
@@ -1,50 +1,60 @@
 import numpy as np
 import matplotlib.pyplot as plt
-#输入数据
-X = np.array([[1,0,0,0,0,0],
-              [1,0,1,0,0,1],
-              [1,1,0,1,0,0],
-              [1,1,1,1,1,1]])
-#标签
-Y = np.array([1,1,-1])
-#权值初始化，3行1列，取值范围-1 到 1
+
+# 输入数据
+X = np.array([[1, 0, 0, 0, 0, 0],
+              [1, 0, 1, 0, 0, 1],
+              [1, 1, 0, 1, 0, 0],
+              [1, 1, 1, 1, 1, 1]])
+# 标签
+Y = np.array([-1, 1, 1, -1])
+# 权值初始化，3行1列，取值范围-1 到 1
 W = (np.random.random(6) - 0.5) * 2
 
 print(W)
-#学习率设置
+# 学习率设置
 lr = 0.11
-#神经网络输出
+# 神经网络输出
 O = 0
+#计算迭代次数
+n = 0
+
 def update():
-    global X,Y,W,lr
-    O = (np.dot(X,W)) #shap:(3,1)
-    W_C = lr * (X.T.dot(Y - O)) / int(X.shape[0])
+    global X, Y, W, lr, n
+    n += 1
+    O = (np.dot(X, W.T))
+    W_C = lr * ((Y - O.T).dot(X)) / int(X.shape[0])
     W = W + W_C
 
-for i in range(100):
+
+for _ in range(1000):
     update()
-    O = np.sign(np.dot(X,W))
-    if(O == Y.T).all():
-        print("Finished")
-        print('epoch',i)
-        break
-#正样本
-x1 = [3,4]
-y1 = [3,4]
-#负样本
-x2 = [1,0]
-y2 = [1,2]
 
-#计算分界线的斜率以及截距
-k = -W[1]/W[2]
-d = -W[0]/W[2]
-print('k=',k)
-print('d=',d)
+# 正样本
+x1 = [0, 1]
+y1 = [1, 0]
+# 负样本
+x2 = [0, 1]
+y2 = [0, 1]
 
-xdata = (0,5)
+
+def calculate(x, root):
+    a = W[3]
+    b = W[2] + x * W[4]
+    c = W[0] + x * W[1] + x * x * W[3]
+    if root == 1:
+        return (-b + np.sqrt(b * b - 4 * a * c)) / (2 * a)
+    if root == 2:
+        return (-b - np.sqrt(b * b - 4 * a * c)) / (2 * a)
+
+
+xdata = np.linspace(-1, 2)
 
 plt.figure()
-plt.plot(xdata,xdata * k +d,'r')
-plt.scatter(x1,y1,c='b')
-plt.scatter(x2,y2,c='y')
+
+plt.plot(xdata, calculate(xdata,1), 'r')
+plt.plot(xdata, calculate(xdata,2), 'r')
+
+plt.plot(x1, y1, 'bo')
+plt.plot(x2, y2, 'yo')
 plt.show()
