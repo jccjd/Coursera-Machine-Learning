89b757effbd0c7f42f35a53a9203e9a740db2811
jccjd <1340944538@qq.com>
2019-02-18 19:20:39 +0800

    week2

--- week-4/code/单层感知器_异或.py
+++ week-4/code/单层感知器_异或.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 X = np.array([[1,0,0],
              [1,0,1],
              [1,1,0],
-             [1,1,1,]]
+             [1,1,1]]
              )
 #标签
 Y = np.array([[-1],
@@ -24,3 +24,32 @@ def update():
     O = np.sign(np.dot(X,W))
     W_C = lr * (X.T.dot(Y-O))/int(X.shape[0])
     W = W + W_C
+for i in range(100):
+    update()
+    print(W)
+    print(i)
+    O = np.sign(np.dot(X,W))
+    if(O == Y).all():
+        print("Finished")
+        print('epoch',i)
+        break
+#正样本
+x1 = [3,4]
+y1 = [3,4]
+#负样本
+x2 = [1,0]
+y2 = [1,2]
+
+#计算分界线的斜率以及截距
+k = -W[1]/W[2]
+d = -W[0]/W[2]
+print('k=',k)
+print('d=',d)
+
+xdata = (0,5)
+
+plt.figure()
+plt.plot(xdata,xdata * k +d,'r')
+plt.scatter(x1,y1,c='b')
+plt.scatter(x2,y2,c='y')
+plt.show()
