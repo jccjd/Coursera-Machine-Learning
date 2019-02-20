103c7573204c8d82be4104806d68f79c4745959b
jccjd <1340944538@qq.com>
2019-02-20 12:08:32 +0800

    week4

--- week-4/code/KNN简单例子.py
+++ week-4/code/KNN简单例子.py
@@ -2,4 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import operator
 
-#已知
+#已知分类数据
+x1 = np.array([3,2,1])
+y1 = np.array([104,100,81])
+x2 = np.array([101,99,98])
+y2 = np.array([10,5,2])
+scatter1 = plt.scatter(x1,y1,c = 'r')
+scatter2 = plt.scatter(x2,y2,c = 'b')
+#未知数据
+x = np.array([18])
+y = np.array([90])
+scatter3 = plt.scatter(x,y,c = 'k')
+plt.legend(handles=[scatter1,scatter2,scatter3],labels=['labelA','labelB','x'],loc='best')
+plt.show()
