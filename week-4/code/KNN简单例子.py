266195018bff95ae60cfde61e457f46a12fa421c
jccjd <1340944538@qq.com>
2019-02-20 12:31:48 +0800

    week4

--- week-4/code/KNN简单例子.py
+++ week-4/code/KNN简单例子.py
@@ -24,4 +24,39 @@ x_data = np.array([[3,104],
                    [8,2]])
 y_data = np.array(['A','A','B','B','B'])
 x_test = np.array([18,90])
-#
+#计算样本数量
+x_data_size = x_data.shape[0]
+print(x_data_size)
+#复制x_test
+np.tile(x_test,(x_data_size,1))
+#计算x_test与每一个样本的差值
+diffMat = np.tile(x_test,(x_data_size,1)) - x_data
+print(diffMat)
+
+sqDiffMat = diffMat ** 2
+print(sqDiffMat)
+
+#求和
+sqDistances = sqDiffMat.sum(axis=1)
+print(sqDistances)
+#开方
+distances = sqDistances ** 0.5
+print(distances)
+#从小到大排序
+sortedDistances = distances.argsort()
+print(sortedDistances)
+
+classCount={}
+#设置k
+k = 5
+for i in range(k):
+    #获取标签
+    votelabel = y_data[sortedDistances[i]]
+    classCount[votelabel] = classCount.get(votelabel,0) + 1
+print(classCount)
+
+sortedClasscount = sorted(classCount.items(),key=operator.itemgetter(1),reverse = True)
+print(sortedClasscount)
+
+
+
