633a4b5c852638bea61b621d592a16704de5fd68
jccjd <1340944538@qq.com>
2019-02-24 09:48:10 +0800

    week8

--- week-8/code/sklearn_dbscan1.py
+++ week-8/code/sklearn_dbscan1.py
@@ -3,10 +3,16 @@ from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
 #载入数据
 data = np.genfromtxt('data1.txt',delimiter=' ')
+
+#训练模型
 model = DBSCAN(eps=1,min_samples=4)
 model.fit(data)
 
 result = model.fit_predict(data)
-print(result)
+# print(result)
 
-#花点
+#画点
+mark = ['or','ob','og','oy','ok','om']
+for i,d in enumerate(data):
+    plt.plot(d[0],d[1],mark[result[i]])
+plt.show()
