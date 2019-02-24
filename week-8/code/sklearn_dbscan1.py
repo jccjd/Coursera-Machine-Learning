74bc22addb01007f05033066d49d42b9fdee210a
jccjd <1340944538@qq.com>
2019-02-24 09:53:13 +0800

    week8

--- week-8/code/sklearn_dbscan1.py
+++ week-8/code/sklearn_dbscan1.py
@@ -5,14 +5,14 @@ import matplotlib.pyplot as plt
 data = np.genfromtxt('data1.txt',delimiter=' ')
 
 #训练模型
-model = DBSCAN(eps=1,min_samples=4)
+model = DBSCAN(eps=1.5,min_samples=4)
 model.fit(data)
 
 result = model.fit_predict(data)
 # print(result)
 
 #画点
-mark = ['or','ob','og','oy','ok','om']
+mark = ['or','ob','og','oy','ok']
 for i,d in enumerate(data):
     plt.plot(d[0],d[1],mark[result[i]])
 plt.show()
