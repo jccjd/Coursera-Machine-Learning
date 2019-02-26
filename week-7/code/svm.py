1fbb7ae3e3357b8f891162635e852297e1d3c459
jccjd <1340944538@qq.com>
2019-02-26 22:03:26 +0800

    week7

--- week-7/code/svm.py
+++ week-7/code/svm.py
@@ -3,5 +3,10 @@ x = [[3,3],[4,3],[1,1]]
 y = [1,1,-1]
 model = svm.SVC(kernel='linear')
 model.fit(x,y)
-
-
+#打印支持向量
+print(model.support_vectors_)
+#第2和第0个点是支持向量
+print(model.support_)
+#有几个支持向量
+print(model.n_support_)
+model.predict([[4,3]])
